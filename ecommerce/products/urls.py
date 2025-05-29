9d6c77e79b25861813eae9a112b827151d6810ec
anonim <anonim@gmail.com>
2025-05-29 16:54:09 +0300

    add files

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -2,10 +2,15 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
+
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
-router.register(r'carts', CartViewSet)
-router.register(r'orders', OrderViewSet)
+router.register(r'carts', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='order')
 
-urlpatterns = []
+urlpatterns = [
+    path('', include(router.urls)),
+    path('carts/<int:cart_id>/add_item/', AddItemToCart.as_view(), name='add-item-to-cart'),
+    path('orders/create_from_cart/', CreateOrderFromCart.as_view(), name='create-order-from-cart'),
+]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -3,62 +3,78 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Category, Product, Cart, CartItem, Order, OrderItem
-from .serializers import (
-    CategorySerializer, ProductSerializer, CartSerializer, CartItemSerializer, OrderSerializer
-)
+from .serializers import *
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    queryset = Category.objects.all()
+class CategoryAdminViewSet(viewsets.ModelViewSet):
+    queryset = category.objects.all()
     serializer_class = CategorySerializer
+    permissions_classes = [permissions.IsAdminInUser]
 
-class ProductViewSet(viewsets.ModelViewSet):
+
+class ProductAdminViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permissions_classes = [permissions.IsAdminInUser]
 
-class CartViewSet(viewsets.ModelViewSet):
-    queryset = Cart.objects.all()
-    serializer_class = CartSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+class OrderAdminViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+# ----------------------------------------------------
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    quearyset = Category.objects.all()
+    serializer_class =CategorySerializer
+    permissions_classes = [permissions.IsAllowAny]
+
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    quearyset = Product.objects.all()
+    serializer_class =ProductSerializer
+    permissions_classes = [permissions.IsAllowAny]
+
+
+class CartViewSet(viewset.ModelViewSet):
+    quearyset = Product.objects.all()
+    serializer_class =ProductSerializer
+    permissions_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Cart.objects.filter(user=self.request.user)
+        return Cart.objects.filter(user = self.request.user)
+
 
 class AddItemToCart(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permissions_classes = [permissions.IsAuthenticated]
 
     def post(self, request, cart_id):
         cart = get_object_or_404(Cart, pk=cart_id, user=request.user)
-        product_id = request.data.get("product_id")
-        quantity = int(request.data.get("quantity", 1))
+        product_id = request.data.get('product_id')
+        quantity = int(request.data.get('quantity', 1))
         product = get_object_or_404(Product, id=product_id)
-        item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+        item, create = CartItem.objects.get_or_create(cart=cart, product=product)
+
         if not created:
-            item.quantity += quantity
+            items.quantity += quantity
         else:
             item.quantity = quantity
         item.save()
-        return Response(CartSerializer(cart).data, status=status.HTTP_200_OK)
+
+        return Response(CategorySerializer(cart).date, status=status.HTTP_200_OK)
+
 
 class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permissions_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return Order.objects.filter(user = self.request.user)
+
 
 class CreateOrderFromCart(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    permissions_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
-        cart = get_object_or_404(Cart, user=request.user)
-        order = Order.objects.create(user=request.user)
-        for item in cart.items.all():
-            OrderItem.objects.create(
-                order=order,
-                product=item.product,
-                quantity=item.quantity,
-                price=item.product.price,
-            )
-        cart.items.all().delete()
-        return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
+        cart = get_object_or_404(Cart, user = request.user)
+
